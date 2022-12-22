68446522a71980a5cbb65382c516d08554d8ba26
橘子你个奥睿智 <51246778+kkive@users.noreply.github.com>
2022-12-22 10:28:20 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+#app.py
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/test',methods=['get','post'])
+
+
+
+
+def hello_world():
+    import os
+    import openai
+    account = request.args.get("ask")
+    openai.api_key="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"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=account,
+        temperature=0,
+        max_tokens=1024,  #  官方为7，建议1024，大小影响答案次数
+        )
+    return (response.choices[0].text).strip()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
